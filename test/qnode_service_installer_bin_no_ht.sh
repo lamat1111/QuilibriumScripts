fd578718ca2fa5b7142819f7a91a6b5872b775b4
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-06 08:12:39 +0200

    Update qnode_service_installer_bin_no_ht.sh

--- test/qnode_service_installer_bin_no_ht.sh
+++ test/qnode_service_installer_bin_no_ht.sh
@@ -129,7 +129,7 @@ fi
 # ==================
 # Set the version number
 
-VERSION=$(cat config/version.go | grep -A 1 "func GetVersion() \[\]byte {" | grep -Eo '0x[0-9a-fA-F]+' | xargs printf "%d.%d.%d")
+VERSION=$(cat $NODE_PATH/config/version.go | grep -A 1 "func GetVersion() \[\]byte {" | grep -Eo '0x[0-9a-fA-F]+' | xargs printf "%d.%d.%d")
 
 # Get the system architecture
 ARCH=$(uname -m)
