74fd95c1a209c03fbd9036f091d076014a754925
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-07 12:47:35 +0200

    Update qnode_service_installer.sh

--- qnode_service_installer.sh
+++ qnode_service_installer.sh
@@ -1,128 +1,8 @@
 #!/bin/bash
 
-# Function to check for updates on GitHub and download the new version if available
-check_for_updates() {
-    echo "Checking for updates..."
-    latest_version=$(curl -s https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/main/qone.sh | md5sum | awk '{print $1}')
-    current_version=$(md5sum $0 | awk '{print $1}')
+# Step 0: Welcome
 
-    if [ "$latest_version" != "$current_version" ]; then
-        echo "A new version is available. Updating..."
-        sleep 1
-        wget -O "$0.tmp" https://github.com/lamat1111/QuilibriumScripts/raw/main/qone.sh
-        chmod +x "$0.tmp"
-        mv -f "$0.tmp" "$0"
-        echo "Update complete. Restarting..."
-        sleep 3
-        exec "$0"
-    else
-        echo "You already have the latest version."
-        sleep 1
-    fi
-}
-
-# Service file path
-SERVICE_FILE="/lib/systemd/system/ceremonyclient.service"
-# User working folder
-HOME=$(eval echo ~$USER)
-# Node path
-NODE_PATH="$HOME/ceremonyclient/node" 
-
-VERSION=$(cat $NODE_PATH/config/version.go | grep -A 1 "func GetVersion() \[\]byte {" | grep -Eo '0x[0-9a-fA-F]+' | xargs printf "%d.%d.%d")
-
-# Get the system architecture
-ARCH=$(uname -m)
-
-if [ "$ARCH" = "x86_64" ]; then
-    EXEC_START="$NODE_PATH/node-$VERSION-linux-amd64"
-elif [ "$ARCH" = "aarch64" ]; then
-    EXEC_START="$NODE_PATH/node-$VERSION-linux-arm64"
-elif [ "$ARCH" = "arm64" ]; then
-    EXEC_START="$NODE_PATH/node-$VERSION-darwin-arm64"
-else
-    echo "❌ Unsupported architecture: $ARCH"
-    exit 1
-fi
-
-# Function to ask for confirmation with an EOF message
-confirm_action() {
-    cat << EOF
-
-$1
-
-Do you want to proceed with "$2"? Type Y or N:
-EOF
-    read -p "> " confirm
-    if [[ "$confirm" =~ ^[Yy]$ ]]; then
-        $3
-        return 0
-    else
-        echo "Action \"$2\" canceled."
-        return 1
-    fi
-}
-
-# Function definitions
-install_prerequisites() {
-    echo "Running installation script for server prerequisites..."
-    wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/quilibriumscripts/master/server_setup.sh | bash
-}
-
-install_node() {
-    echo "Running installation script for Quilibrium Node..."
-    wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/master/qnode_service_installer.sh | bash
-}
-
-configure_grpcurl() {
-    echo "Running configuration script for gRPCurl..."
-    wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/quilibriumscripts/master/tools/qnode_gRPC_calls_setup.sh | bash
-}
-
-update_node() {
-    echo "Running update script for Quilibrium Node..."
-    wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/master/qnode_service_update.sh | bash
-}
-
-check_visibility() {
-    echo "Checking visibility of Quilibrium Node..."
-    wget -O - https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/main/tools/qnode_visibility_check.sh | bash
-}
-
-node_info() {
-    echo "Displaying information about Quilibrium Node..."
-    cd "$NODE_PATH" && "$EXEC_START" -node-info
-}
-
-node_logs() {
-    echo "Displaying logs of Quilibrium Node..."
-    sudo journalctl -u ceremonyclient.service -f --no-hostname -o cat
-}
-
-restart_node() {
-    echo "Restarting Quilibrium Node service..."
-    service ceremonyclient restart
-}
-
-stop_node() {
-    echo "Stopping Quilibrium Node service..."
-    service ceremonyclient stop
-}
-
-peer_manifest() {
-    echo "Checking peer manifest (Difficulty metric)..."
-    wget --no-cache -O - https://raw.githubusercontent.com/lamat1111/quilibriumscripts/main_new/tools/qnode_peermanifest_checker.sh | bash
-}
-
-node_version() {
-    echo "Displaying Quilibrium Node version..."
-    journalctl -u ceremonyclient -r --no-hostname -n 1 -g "Quilibrium Node" -o cat
-}
-
-# Main menu
-while true; do
-    clear
-    
-    cat << "EOF"
+cat << "EOF"
 
                   QQQQQQQQQ       1111111   
                 QQ:::::::::QQ    1::::::1   
@@ -145,64 +25,163 @@ while true; do
 
 
 ===================================================================
-                      ✨ QNODE QUICKSTART ✨
+                 ✨ QNODE SERVICE INSTALLER ✨
 ===================================================================
-         Follow the guide at https://docs.quilibrium.one
+This script will install your Quilibrum node as a service.
+It will run your node from the release_autostart.sh file.
 
-                      Made with 🔥 by LaMat
+Be sure to run the 'Server Setup' script first.
+Follow the guide at https://docs.quilibrium.one
+
+Made with 🔥 by LaMat - https://quilibrium.one
 ====================================================================
+
+Processing... ⏳
 
 EOF
 
-    echo "Choose an option:"
-    echo ""
-    echo "If you want install a new node choose option 1, and then 2"
-    echo ""
-    echo "1) Prepare your server"
-    echo "2) Install Node"
-    echo "------------------------"
-    echo "3) Update Node"
-    echo "4) Set up gRPCurl"
-    echo "5) Check Visibility"
-    echo "6) Node Info"
-    echo "7) Node Logs (CTRL+C to detach)"
-    echo "8) Restart Node"
-    echo "9) Stop Node"
-    echo "10) Peer manifest (Difficulty metric)"
-    echo "11) Node Version"
-    echo "e) Exit"
+sleep 7  # Add a 7-second delay
 
-    read -p "Enter your choice: " choice
-    action_performed=0
 
-    case $choice in
-        1) confirm_action "This action will install the necessary prerequisites for your server.
-If this is the first time you install a Quilibrium node I suggest you 
-to follow the online guide instead at: https://docs.quilibrium.one/" "Prepare your server" install_prerequisites && action_performed=1 ;;
-        2) confirm_action "This action will install the node on your server.
-If this is the first time you install a Quilibrium node I suggest you 
-to follow the online guide instead at: https://docs.quilibrium.one/
-Ensure that your server meets all the requirements and that you have already prepared your server via Step 1." "Install Node" install_node && action_performed=1 ;;
-        3) confirm_action "This action will update your node.
-Only use this if you have installed the node via the guide at https://docs.quilibrium.one/" "Update Node" update_node && action_performed=1 ;;
-        4) confirm_action "This action will make some edits to your config.yml to enable communication with the network.
-If this is a fresh node installation, let the node run for 30 minutes before doing this." "Set up gRPCurl" configure_grpcurl && action_performed=1 ;;
-        5) check_visibility && action_performed=1 ;;
-        6) node_info && action_performed=1 ;;
-        7) node_logs && action_performed=1 ;;
-        8) restart_node && action_performed=1 ;;
-        9) stop_node && action_performed=1 ;;
-        10) confirm_action "This action will check the peer manifest to provide information about the difficulty metric score of your node.
-It only works after 15-30 minutes that the node has been running." "Peer manifest" peer_manifest && action_performed=1 ;;
-        11) node_version && action_performed=1 ;;
-        e) break ;;
-        *) echo "Invalid option, please try again." ;;
-    esac
+# Exit on any error
+set -e
 
-    if [ $action_performed -eq 1 ]; then
-        read -n 1 -s -r -p "Press any key to continue"
+# Step 1: Define a function for displaying exit messages
+exit_message() {
+    echo "❌ Oops! There was an error during the script execution and the process stopped. No worries!"
+    echo "🔄 You can try to run the script from scratch again."
+    echo "🛠️ If you still receive an error, you may want to proceed manually, step by step instead of using the auto-installer."
+}
+
+# Function to install a package if it is not already installed
+install_package() {
+    echo "⏳ Installing $1..."
+    if apt-get install -y $1; then
+        echo "✅ $1 installed successfully."
+    else
+        echo "❌ Failed to install $1. You will have to do this manually."
     fi
-done
+}
 
-# Check for updates before displaying the menu
-check_for_updates
+# Install cpulimit
+install_package cpulimit
+
+# Install gawk
+install_package gawk
+
+echo "✅ cpulimit and gawk are installed and up to date."
+
+
+# Step 2: Set a trap to call exit_message on any error
+trap exit_message ERR
+
+# Step 3: Backup existing configuration files if they exist
+if [ -d ~/ceremonyclient ]; then
+    mkdir -p ~/backup/qnode_keys
+    [ -f ~/ceremonyclient/node/.config/keys.yml ] && cp ~/ceremonyclient/node/.config/keys.yml ~/backup/qnode_keys/ && echo "✅ Backup of keys.yml created in ~/backup/qnode_keys folder"
+    [ -f ~/ceremonyclient/node/.config/config.yml ] && cp ~/ceremonyclient/node/.config/config.yml ~/backup/qnode_keys/ && echo "✅ Backup of config.yml created in ~/backup/qnode_keys folder"
+fi
+
+# Step 4: Download Ceremonyclient
+echo "⏳ Downloading Ceremonyclient"
+sleep 1  # Add a 1-second delay
+cd ~
+if [ -d "ceremonyclient" ]; then
+  echo "Directory ceremonyclient already exists, skipping git clone..."
+else
+  attempt=0
+  max_attempts=3
+  while [ $attempt -lt $max_attempts ]; do
+    if git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git; then
+      echo "✅ Successfully cloned from https://source.quilibrium.com/quilibrium/ceremonyclient.git"
+      break
+    elif git clone https://git.quilibrium-mirror.ch/agostbiro/ceremonyclient.git; then
+      echo "✅ Successfully cloned from https://git.quilibrium-mirror.ch/agostbiro/ceremonyclient.git"
+      break
+    elif git clone https://github.com/QuilibriumNetwork/ceremonyclient.git; then
+      echo "✅ Successfully cloned from https://github.com/QuilibriumNetwork/ceremonyclient.git"
+      break
+    else
+      attempt=$((attempt+1))
+      echo "Git clone failed (attempt $attempt of $max_attempts), retrying..."
+      sleep 2
+    fi
+  done
+
+  if [ $attempt -ge $max_attempts ]; then
+    echo "❌ Error: Failed to clone the repository after $max_attempts attempts." >&2
+    exit 1
+  fi
+fi
+
+cd ~/ceremonyclient/
+git checkout release
+
+# Set up environment variables (redundant but solves the command go not found error)
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# Step 4.1: Build Ceremonyclient qClient
+echo "⏳ Building qClient..."
+sleep 1  # Add a 1-second delay
+cd ~/ceremonyclient/client
+GOEXPERIMENT=arenas go build -o qclient main.go
+
+# Step 5: Determine the ExecStart line based on the architecture
+# Get the current user's home directory
+HOME=$(eval echo ~$USER)
+
+# Use the home directory in the path
+NODE_PATH="$HOME/ceremonyclient/node"
+EXEC_START="$NODE_PATH/release_autorun.sh"
+
+# Step 6: Create Ceremonyclient Service
+echo "⏳ Creating Ceremonyclient Service"
+sleep 2  # Add a 2-second delay
+
+# Check if the file exists before attempting to remove it
+if [ -f "/lib/systemd/system/ceremonyclient.service" ]; then
+    rm /lib/systemd/system/ceremonyclient.service
+    echo "ceremonyclient.service file removed."
+else
+    echo "ceremonyclient.service file does not exist. No action taken."
+fi
+
+sudo tee /lib/systemd/system/ceremonyclient.service > /dev/null <<EOF
+[Unit]
+Description=Ceremony Client Go App Service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+WorkingDirectory=$NODE_PATH
+ExecStart=$EXEC_START
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 7: Start the ceremonyclient service
+echo "✅ Starting Ceremonyclient Service"
+
+sleep 2  # Add a 2-second delay
+sudo systemctl daemon-reload
+sudo systemctl enable ceremonyclient
+sudo service ceremonyclient start
+
+# Step 8: Final messages
+echo "🎉 Now your node is starting!"
+echo "🕒 Let it run for at least 30 minutes to generate your keys."
+echo ""
+echo "🔐 You can logout of your server if you want and login again later."
+echo "🔒 After 30 minutes, backup your keys.yml and config.yml files."
+echo "ℹ️ More info about this in the online guide: https://docs.quilibrium.one"
+echo ""
+echo "📜 Now I will show the node log below..."
+echo "To exit the log, just type CTRL +C."
+
+# Step 9: See the logs of the ceremonyclient service
+sleep 5  # Add a 5-second delay
+sudo journalctl -u ceremonyclient.service -f --no-hostname -o cat
