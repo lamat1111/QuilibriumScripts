d977e7a62a80744d3c0a675b4fba294595e56edc
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-08 19:59:49 +0200

    Update qone.sh
    added emojis ⌛️

--- qone.sh
+++ qone.sh
@@ -2,13 +2,13 @@
 
 # Function to check for updates on GitHub and download the new version if available
 check_for_updates() {
-    echo "⚙️ Checking for updates..."
+    echo "⌛️   Checking for updates..."
     sleep 1
     latest_version=$(curl -s https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/main/qone.sh | md5sum | awk '{print $1}')
     current_version=$(md5sum $0 | awk '{print $1}')
 
     if [ "$latest_version" != "$current_version" ]; then
-        echo "⚙️ A new version is available. Updating..."
+        echo "⌛️   A new version is available. Updating..."
 	sleep 1
         wget -O "$0.tmp" https://github.com/lamat1111/QuilibriumScripts/raw/main/qone.sh
         chmod +x "$0.tmp"
