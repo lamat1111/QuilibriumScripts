453d55d44f8cdc0b36d2104ee820b688d6c34331
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-09 12:48:18 +0200

    Update qone_setup.sh

--- qone_setup.sh
+++ qone_setup.sh
@@ -110,13 +110,6 @@ echo "✅ Setup complete!"
 echo "You can now use 'Q1', 'q1', or 'qone' to launch the Node Quickstart Menu."
 echo "The menu will also load automatically every time you log in."
 echo ""
-#echo "🟢 To open the menu, type 'qone' and ENTER 🟢"
 sleep 2
 
-#echo "⌛️ Opening the menu..."
-#sleep 5
-# Execute qone.sh
-#~/qone.sh
-#if [ $? -ne 0 ]; then
-#    echo "❌ Error: Failed to execute qone.sh. Try to run './qone.sh' manually"
-#fi
+
