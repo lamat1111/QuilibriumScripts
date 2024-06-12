d696ffbfbc0da5fa8c94caecb529014dbcecc436
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-12 16:34:06 +0200

    Update qone_setup.sh

--- qone_setup.sh
+++ qone_setup.sh
@@ -12,8 +12,16 @@ if ! command -v wget &> /dev/null; then
     fi
 fi
 
+# Remove existing qone.sh if it exists
+if [ -e ~/qone.sh ]; then
+    rm ~/qone.sh
+    if [ $? -ne 0 ]; then
+        echo "❌ Error: Failed to remove qone.sh"
+    fi
+fi
+
 # Download the qone.sh script
-wget -O ~/qone.sh https://github.com/lamat1111/QuilibriumScripts/raw/main/qone.sh
+wget -O ~/qone.sh https://github.com/lamat1111/QuilibriumScripts/raw/main/testing/qone.sh
 if [ $? -ne 0 ]; then
     echo "❌ Error: Failed to download qone.sh script."
     exit 1
@@ -27,47 +35,18 @@ if [ $? -ne 0 ]; then
     # Continue execution even if chmod fails
 fi
 
-#!/bin/bash
-
 # Check if the qone.sh setup section is already present in .bashrc
 if grep -Fxq "# === qone.sh setup ===" ~/.bashrc; then
     echo "⚠️ The qone.sh setup section is already present in .bashrc."
     echo "Skipping the setup steps..."
 else
-    # Check if wget is installed, and install it if necessary
-    if ! command -v wget &> /dev/null; then
-        echo "⌛️ wget is not installed. Installing wget..."
-        sleep 1
-        sudo apt update
-        sudo apt install wget -y
-        if [ $? -ne 0 ]; then
-            echo "❌ Error: Failed to install wget."
-            exit 1
-        fi
-    fi
-
-    # Download the qone.sh script
-    wget -O ~/qone.sh https://github.com/lamat1111/QuilibriumScripts/raw/main/qone.sh
-    if [ $? -ne 0 ]; then
-        echo "❌ Error: Failed to download qone.sh script."
-        exit 1
-    fi
-
-    # Make qone.sh executable
-    chmod +x ~/qone.sh
-    if [ $? -ne 0 ]; then
-        echo "❌ Error: Failed to make qone.sh executable. You will have to do this manually."
-        sleep 1
-        # Continue execution even if chmod fails
-    fi
-
     # Check if there's already a section for another script in .bashrc
     if grep -q "# === [^=]*setup ===" ~/.bashrc; then
         echo "⚠️ Warning: Another script seems to be already set up to run on login."
         echo "To avoid conflicts, qone.sh will not be executed on login, but aliases will be set up."
         # Define the section to add in .bashrc without the execution line
         bashrc_section=$(cat << 'EOF'
-
+        
 # === qone.sh setup ===
 # The following lines are added to create aliases for qone.sh
 alias qone='~/qone.sh'
@@ -79,7 +58,7 @@ EOF
     else
         # Define the section to add in .bashrc with the execution line
         bashrc_section=$(cat << 'EOF'
-
+        
 # === qone.sh setup ===
 # The following lines are added to run qone.sh on login and create aliases for qone.sh
 ~/qone.sh #this runs .qone on login
@@ -100,12 +79,5 @@ EOF
     fi
 fi
 
-# Reload .bashrc to apply changes
-source ~/.bashrc
-if [ $? -ne 0 ]; then
-    echo "❌ Error: Failed to reload .bashrc. No worries, this is optional."
-    sleep 1
-    # Continue execution even if reloading .bashrc fails
-fi
 
 
