48320e7396c9b570ecfb564d77003cc436b4d2ae
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-16 17:10:03 +0200

    changed code to fetch node binary path

--- tools/qnode_balance_checker.sh
+++ tools/qnode_balance_checker.sh
@@ -8,12 +8,10 @@ SCRIPT_VERSION="1.5.4"
 
 # Function to check for newer script version
 check_for_updates() {
-    #echo "⚙️ Checking for script updates..."
     LATEST_VERSION=$(wget -qO- "https://github.com/lamat1111/QuilibriumScripts/raw/main/tools/qnode_balance_checker.sh" | grep 'SCRIPT_VERSION="' | head -1 | cut -d'"' -f2)
     if [ "$SCRIPT_VERSION" != "$LATEST_VERSION" ]; then
         wget -O ~/scripts/qnode_balance_checker.sh "https://github.com/lamat1111/QuilibriumScripts/raw/main/tools/qnode_balance_checker.sh"
         chmod +x ~/scripts/qnode_balance_checker.sh
-        #echo "✅ New version downloaded: V $SCRIPT_VERSION."
         sleep 1
     fi
 }
@@ -21,46 +19,49 @@ check_for_updates() {
 # Check for updates and update if available
 check_for_updates
 
-# Function to retrieve the node binary filename dynamically
-get_node_binary_filename() {
-    local version_file="$HOME/ceremonyclient/node/config/version.go"
-    local version_hex=$(grep -A 1 "func GetVersion() \[\]byte {" "$version_file" | grep -Eo '0x[0-9a-fA-F]+' | xargs printf "%d.%d.%d")
-
-    local arch=$(uname -m)
-    local node_binary
-
-    if [ "$arch" = "x86_64" ]; then
-        node_binary="node-$version_hex-linux-amd64"
-    elif [ "$arch" = "aarch64" ]; then
-        node_binary="node-$version_hex-linux-arm64"
-    elif [ "$arch" = "arm64" ]; then
-        node_binary="node-$version_hex-darwin-arm64"
+# Function to fetch node binary and set NODE_BINARY variable
+fetch_node_binary() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        release_os="linux"
+        release_arch=$(uname -m)
+        if [[ "$release_arch" == "aarch64" ]]; then
+            release_arch="arm64"
+        else
+            release_arch="amd64"
+        fi
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        release_os="darwin"
+        release_arch="arm64"
     else
-        echo "Unsupported architecture: $arch"
+        echo "Unsupported OS for releases, please build from source"
         exit 1
     fi
 
-    echo "$node_binary"
+    files=$(curl -s https://releases.quilibrium.com/release | grep "$release_os-$release_arch")
+
+    for file in $files; do
+        version=$(echo "$file" | cut -d '-' -f 2)
+        break
+    done
+
+    NODE_BINARY=node-$version-$release_os-$release_arch
+    echo "$NODE_BINARY"
 }
 
 # Function to get the unclaimed balance
 get_unclaimed_balance() {
     local node_directory="$HOME/ceremonyclient/node"
     local NODE_BINARY
-    NODE_BINARY=$(get_node_binary_filename)
+    NODE_BINARY=$(fetch_node_binary)
     local node_command="./$NODE_BINARY -balance"
     
-    # Run node command and capture output
     local output
     output=$(cd "$node_directory" && $node_command 2>&1)
     
-    # Parse output to find unclaimed balance
     local balance
     balance=$(echo "$output" | grep "Unclaimed balance" | awk '{print $3}' | sed 's/[^0-9.]//g')
     
-    # Check if balance is a valid number
     if [[ "$balance" =~ ^[0-9.]+$ ]]; then
-        # Format balance to 2 decimal places
         balance=$(printf "%.2f" "$balance")
         echo "$balance"
     else
@@ -74,12 +75,10 @@ write_to_csv() {
     local filename="$HOME/scripts/balance_log.csv"
     local data="$1"
 
-    # Check if file exists to determine if headers need to be written
     if [ ! -f "$filename" ] || [ ! -s "$filename" ]; then
-        echo "time,balance,increase" > "$filename"
+        echo "time,balance" > "$filename"
     fi
 
-    # Append data to CSV
     echo "$data" >> "$filename"
 }
 
@@ -94,32 +93,9 @@ main() {
     if [ -n "$balance" ]; then
         local filename="$HOME/scripts/balance_log.csv"
         
-        # Read the last recorded balance and increase from CSV
-        local last_record
-        last_record=$(tail -n 1 "$filename" 2>/dev/null)
-        local previous_balance=0
-        local previous_increase=0
+        local data_to_write="$current_time,$balance"
         
-        if [ -n "$last_record" ]; then
-            previous_balance=$(echo "$last_record" | awk -F ',' '{print $2}')
-            previous_increase=$(echo "$last_record" | awk -F ',' '{print $3}')
-        fi
-        
-        # Calculate increase in balance since last recorded balance
-        local increase=$(echo "$balance - $previous_increase" | bc)
-        
-        # Format balance and increase to required precision
-        local formatted_balance=$(printf "%.2f" "$balance")
-        local formatted_increase=$(printf "%.2f" "$increase")
-        
-        # Print data
-        local data_to_write="$current_time,$formatted_balance,$formatted_increase"
-        
-        # Write to CSV file
-        if [ ! -f "$filename" ] || [ ! -s "$filename" ]; then
-            echo "time,balance,increase" > "$filename"
-        fi
-        echo "$data_to_write" >> "$filename"
+        write_to_csv "$data_to_write"
     fi
 }
 
