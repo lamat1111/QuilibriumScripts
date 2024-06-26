01c28c9c112fd86610e2e628246060dd0fe94142
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-06-26 16:13:32 +0200

    new version number

--- qone.sh
+++ qone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the version number here
-SCRIPT_VERSION="1.7.1"
+SCRIPT_VERSION="1.7.2"
 
 # Function to check if wget is installed, and install it if it is not
 check_wget() {
