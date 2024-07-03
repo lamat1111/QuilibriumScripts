94fb9aab3474fa5adcdf798dc390598d233eeb33
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-07-03 19:39:07 +0200

    gti clone switched to github

--- qnode_service_installer.sh
+++ qnode_service_installer.sh
@@ -89,7 +89,7 @@ cd ~
 if [ -d "ceremonyclient" ]; then
   echo "Directory ceremonyclient already exists, skipping git clone..."
 else
-  until git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git || git clone https://git.quilibrium-mirror.ch/agostbiro/ceremonyclient.git; do
+  until git clone https://github.com/QuilibriumNetwork/ceremonyclient.git || git clone https://source.quilibrium.com/quilibrium/ceremonyclient.git; do
     echo "Git clone failed, retrying..."
     sleep 2
   done
