b8db5a8d224efa2955671788dc1e44b949cb94a3
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-10-13 17:39:04 +0200

    a

--- tools/qnode_download_node_binary.sh
+++ tools/qnode_download_node_binary.sh
@@ -33,7 +33,7 @@ cd ~/ceremonyclient/node
 # Download each file
 for file in $RELEASE_FILES; do
     echo "Downloading $file..."
-    wget "https://releases.quilibrium.com/$file"
+    curl -L -o "$file" "https://releases.quilibrium.com/$file"
     
     # Check if the download was successful
     if [ $? -eq 0 ]; then
