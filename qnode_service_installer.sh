14a70acc104b4fc33f07f3e3a6134ebfa57e04aa
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-10-13 17:06:21 +0200

    node binary download - added

--- qnode_service_installer.sh
+++ qnode_service_installer.sh
@@ -167,7 +167,7 @@ else
 fi
 
 #==========================
-# DOWNLOAD NODE
+# CEREMONYCLIENT REPO DOWNLOAD
 #==========================
 
 # Download Ceremonyclient
@@ -192,13 +192,73 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 #==========================
+# NODE BINARY DOWNLOAD
+#==========================
+
+get_os_arch() {
+    local os=$(uname -s | tr '[:upper:]' '[:lower:]')
+    local arch=$(uname -m)
+
+    case "$os" in
+        linux|darwin) ;;
+        *) echo "Unsupported operating system: $os" >&2; return 1 ;;
+    esac
+
+    case "$arch" in
+        x86_64|amd64) arch="amd64" ;;
+        arm64|aarch64) arch="arm64" ;;
+        *) echo "Unsupported architecture: $arch" >&2; return 1 ;;
+    esac
+
+    echo "${os}-${arch}"
+}
+
+# Base URL for the Quilibrium releases
+RELEASE_FILES_URL="https://releases.quilibrium.com/release"
+
+# Get the current OS and architecture
+OS_ARCH=$(get_os_arch)
+
+# Fetch the list of files from the release page
+RELEASE_FILES=$(curl -s $RELEASE_FILES_URL | grep -oE "node-[0-9]+\.[0-9]+\.[0-9]+-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
+
+# Change to the download directory
+cd ~/ceremonyclient/node
+
+# Download each file
+for file in $RELEASE_FILES; do
+    echo "Downloading $file..."
+    wget "https://releases.quilibrium.com/$file"
+    
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "Successfully downloaded $file"
+        # Check if the file is the base binary (without .dgst or .sig suffix)
+        if [[ $file =~ ^node-[0-9]+\.[0-9]+\.[0-9]+-${OS_ARCH}$ ]]; then
+            echo "Making $file executable..."
+            chmod +x "$file"
+            if [ $? -eq 0 ]; then
+                echo "Successfully made $file executable"
+            else
+                echo "Failed to make $file executable"
+            fi
+        fi
+    else
+        echo "Failed to download $file"
+    fi
+    
+    echo "------------------------"
+done
+
+echo "✅  Node binary download completed."
+
+#==========================
 # DOWNLOAD QCLIENT
 #==========================
 
-# Building qClient binary
 if [ -n "$QCLIENT_BINARY" ]; then
     echo "⏳ Downloading qClient..."
-    sleep 1  # Add a 1-second delay
+    sleep 1  
     cd ~/ceremonyclient/client
 
     if ! wget https://releases.quilibrium.com/$QCLIENT_BINARY; then
