5fbef2defb4d8bb550bc2615f8406e4cd8161636
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-10-14 10:59:16 +0200

    a

--- tools/qclient_install.sh
+++ tools/qclient_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#Comment out for automatic creation of the qclient version
-#QCLIENT_VERSION=2.0.0
+set -e
 
-# Determine the ExecStart line based on the architecture
-ARCH=$(uname -m)
-OS=$(uname -s)
+# Set BASE_URL
+BASE_URL="https://releases.quilibrium.com"
 
+# Determine the OS and architecture
 get_os_arch() {
     local arch=$(uname -m)
     local os=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -23,33 +22,26 @@ get_os_arch() {
     echo "${os}-${arch}"
 }
 
+OS_ARCH=$(get_os_arch)
+
 # Determine qclient latest version
-# Check if QCLIENT_VERSION is empty
 if [ -z "$QCLIENT_VERSION" ]; then
-    QCLIENT_VERSION=$(curl -s https://releases.quilibrium.com/qclient-release | grep -E "^qclient-[0-9]+(\.[0-9]+)*" | sed 's/^qclient-//' | cut -d '-' -f 1 |  head -n 1)
+    QCLIENT_VERSION=$(curl -s "$BASE_URL/qclient-release" | grep -E "^qclient-[0-9]+(\.[0-9]+)*" | sed 's/^qclient-//' | cut -d '-' -f 1 | head -n 1)
     if [ -z "$QCLIENT_VERSION" ]; then
         echo "❌ Error: Unable to determine QCLIENT_VERSION automatically."
         exit 1
     else
         echo "✅ Automatically determined QCLIENT_VERSION: $QCLIENT_VERSION"
-        echo
     fi
 else
     echo "✅ Using specified QCLIENT_VERSION: $QCLIENT_VERSION"
-    echo
 fi
-
-# Get the current OS and architecture
-OS_ARCH=$(get_os_arch)
 
 # Set the QCLIENT_BINARY based on OS_ARCH
 QCLIENT_BINARY="qclient-$QCLIENT_VERSION-$OS_ARCH"
 
-# Set the BASE_URL
-BASE_URL="https://releases.quilibrium.com"
-
 # Fetch the list of files from the release page
-FILES=$(curl -s $BASE_URL | grep -oE "qclient-$QCLIENT_VERSION-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
+FILES=$(curl -s "$BASE_URL" | grep -oE "qclient-$QCLIENT_VERSION-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
 
 # Create and change to the download directory
 mkdir -p ~/ceremonyclient/client
@@ -60,7 +52,6 @@ for file in $FILES; do
     echo "Downloading $file..."
     wget -O "$file" "$BASE_URL/$file"
     
-    # Check if the download was successful
     if [ $? -eq 0 ]; then
         echo "Successfully downloaded $file"
     else
@@ -77,4 +68,17 @@ if [ -f "$QCLIENT_BINARY" ]; then
     echo "✅ qClient binary downloaded and configured successfully."
 else
     echo "❌ Error: qClient binary not found. Please check the download process."
+    ls -la  # List directory contents for debugging
 fi
+
+# Clean up old files
+echo "Cleaning up old files..."
+find . -type f -name "qclient-*" ! -name "$QCLIENT_BINARY" -delete
+echo "✅ Cleanup completed."
+
+# Print debug information
+echo "Debug Information:"
+echo "OS_ARCH: $OS_ARCH"
+echo "QCLIENT_VERSION: $QCLIENT_VERSION"
+echo "QCLIENT_BINARY: $QCLIENT_BINARY"
+echo "FILES: $FILES"
