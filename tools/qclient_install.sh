4a60175c65f84030807bd4c3bf84c3d88a5244f4
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-10-14 10:47:46 +0200

    qclient download signatures - fixed

--- tools/qclient_install.sh
+++ tools/qclient_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Comment out for automatic creation of the qclient version
-#QCLIENT_VERSION=1.4.19.1
+#QCLIENT_VERSION=2.0.0
 
 # Determine the ExecStart line based on the architecture
 ARCH=$(uname -m)
@@ -26,7 +26,6 @@ fi
 # Determine the binary names based on the architecture and OS
 if [ "$ARCH" = "x86_64" ]; then
     if [ "$OS" = "Linux" ]; then
-        GO_BINARY="go1.22.4.linux-amd64.tar.gz"
         [ -n "$QCLIENT_VERSION" ] && QCLIENT_BINARY="qclient-$QCLIENT_VERSION-linux-amd64"
     elif [ "$OS" = "Darwin" ]; then
         GO_BINARY="go1.22.4.darwin-amd64.tar.gz"
@@ -34,10 +33,8 @@ if [ "$ARCH" = "x86_64" ]; then
     fi
 elif [ "$ARCH" = "aarch64" ]; then
     if [ "$OS" = "Linux" ]; then
-        GO_BINARY="go1.22.4.linux-arm64.tar.gz"
         [ -n "$QCLIENT_VERSION" ] && QCLIENT_BINARY="qclient-$QCLIENT_VERSION-linux-arm64"
     elif [ "$OS" = "Darwin" ]; then
-        GO_BINARY="go1.22.4.darwin-arm64.tar.gz"
         [ -n "$QCLIENT_VERSION" ] && QCLIENT_BINARY="qclient-$QCLIENT_VERSION-darwin-arm64"
     fi
 else
@@ -45,25 +42,32 @@ else
     exit 1
 fi
 
-# Building qclient binary
-if [ -n "$QCLIENT_BINARY" ]; then
-    echo "⏳ Downloading qclient..."
-    sleep 1  # Add a 1-second delay
-    cd ~/ceremonyclient/client
 
-    if ! wget https://releases.quilibrium.com/$QCLIENT_BINARY; then
-        echo "❌ Error: Failed to download qclient binary."
-        echo
+# Get the current OS and architecture
+OS_ARCH=$(get_os_arch)
+
+# Fetch the list of files from the release page
+FILES=$(curl -s $BASE_URL | grep -oE "qclient-[0-9]+\.[0-9]+\.[0-9]+-${OS_ARCH}(\.dgst)?(\.sig\.[0-9]+)?")
+
+# Change to the download directory
+cd ~/ceremonyclient/client
+
+# Download each file
+for file in $FILES; do
+    echo "Downloading $file..."
+    wget "https://releases.quilibrium.com/$file"
+    
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "Successfully downloaded $file"
     else
-        echo "✅ qclient binary downloaded successfully."
-        echo
-        mv $QCLIENT_BINARY qclient
-        echo "$QCLIENT_BINARY renamed to 'qclient'"
-        chmod +x qclient
-        echo
-        echo "✅ qclient is ready."
-        echo
+        echo "❌ Error: Failed to download $file"
+        echo "Your node will still work, but you'll need to install the qclient manually later if needed."
     fi
-else
-    echo "ℹ❌ Error: Skipping qclient download as QCLIENT_BINARY could not be determined."
-fi
+    
+    echo "------------------------"
+done
+
+mv $QCLIENT_BINARY qclient
+chmod +x qclient
+echo "✅ qClient binary downloaded and configured successfully."
