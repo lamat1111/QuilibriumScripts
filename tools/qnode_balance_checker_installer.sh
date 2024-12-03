d7b06c830d11a6c6bbd74125f75a07784bc93c12
LaMat <65860122+lamat1111@users.noreply.github.com>
2024-12-03 11:10:00 +0100

    a

--- tools/qnode_balance_checker_installer.sh
+++ tools/qnode_balance_checker_installer.sh
@@ -33,36 +33,36 @@ sleep 7
 # you will need to manually change this variable at the beginning of the script:
 # '~/scripts/qnode_balance_checker.sh'
 
-#echo "⚙️ Installing Python 3 and pip3..."
+#echo "Installing Python 3 and pip3..."
 #sudo apt install -y python3 python3-pip > /dev/null || { echo "❌ Failed to install Python 3 and pip3."; exit 1; } 
 #sleep 1
 
-echo "⚙️ Removing existing script if it exists..."
+echo "Removing existing script if it exists..."
 echo
 rm -f $HOME/scripts/qnode_balance_checker.sh
 sleep 1
 
-echo "⚙️ Creating directory for scripts..."
+echo "Creating directory for scripts..."
 mkdir -p $HOME/scripts
 sleep 1
 
-echo "⚙️ Downloading new script..."
+echo "Downloading new script..."
 curl -s -o "$HOME/scripts/qnode_balance_checker.sh" https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/main/tools/qnode_balance_checker.sh
 sleep 1
 
-echo "⚙️ Setting executable permissions for the script..."
+echo "Setting executable permissions for the script..."
 chmod +x $HOME/scripts/qnode_balance_checker.sh
 sleep 1
 
-echo "⚙️ Checking if a cronjob exists for qnode_balance_checker.py and deleting it if found..."
+echo "Checking if a cronjob exists for qnode_balance_checker.py and deleting it if found..."
 crontab -l | grep -v "qnode_balance_checker.sh" | crontab -
 sleep 1
 
-echo "⚙️ Setting up cronjob to run the script once every hour..."
+echo "Setting up cronjob to run the script once every hour..."
 (crontab -l ; echo "0 * * * * $HOME/scripts/qnode_balance_checker.sh") | crontab -
 sleep 1
 
-echo "⚙️ Installing the balance log downloader script..."
+echo "Installing the balance log downloader script..."
 curl -s -o "$HOME/scripts/qnode_balance_log_download.sh" https://raw.githubusercontent.com/lamat1111/QuilibriumScripts/main/tools/qnode_balance_log_download.sh
 chmod +x ~/scripts/qnode_balance_log_download.sh
 
